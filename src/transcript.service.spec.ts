839f6fd1d21de4e268a69fb5e2651687225a0a08
Rob Simmons <robsimmons@gmail.com>
2026-01-26 13:26:13 -0500

    Adapt to keyv

--- src/transcript.service.spec.ts
+++ src/transcript.service.spec.ts
@@ -24,49 +24,49 @@ describe(`addStudent()`, () => {
 });
 
 describe(`getTranscript()`, () => {
-  it(`should return an empty transcript for a new student`, () => {
-    const id = db.addStudent("Carol");
-    expect(db.getTranscript(id)).toStrictEqual({
+  it(`should return an empty transcript for a new student`, async () => {
+    const id = await db.addStudent("Carol");
+    expect(await db.getTranscript(id)).toStrictEqual({
       student: { studentName: "Carol", studentID: id },
       grades: [],
     });
   });
 
-  it(`should throw an error for a non-existent student`, () => {
-    const id = db.addStudent("Carol");
-    expect(() => db.getTranscript(id + 1)).toThrow();
+  it(`should throw an error for a non-existent student`, async () => {
+    const id = await db.addStudent("Carol");
+    await expect(db.getTranscript(id + 1)).rejects.toThrow();
   });
 });
 
 describe(`addGrade()`, () => {
-  it(`should successfully add a new element to an empty transcript`, () => {
-    const id = db.addStudent("Carol");
-    db.addGrade(id, "Math", 91);
-    expect(db.getTranscript(id)).toStrictEqual({
+  it(`should successfully add a new element to an empty transcript`, async () => {
+    const id = await db.addStudent("Carol");
+    await db.addGrade(id, "Math", 91);
+    expect(await db.getTranscript(id)).toStrictEqual({
       student: { studentName: "Carol", studentID: id },
       grades: [{ course: "Math", grade: 91 }],
     });
   });
 
-  it(`should throw if given an invalid id`, () => {
+  it(`should throw if given an invalid id`, async () => {
     // All IDs are invalid in the initial database... including 1.5
-    expect(() => db.addGrade(1.5, "Math", 91)).toThrow();
+    await expect(db.addGrade("4", "Math", 91)).rejects.toThrow();
   });
 
-  it(`should attach different grades to different students`, () => {
-    const id1 = db.addStudent("Carol");
-    const id2 = db.addStudent("Darol");
-    db.addGrade(id1, "Math", 91);
-    db.addGrade(id2, "Math", 87);
-    expect(db.getTranscript(id1)?.grades).toStrictEqual([{ course: "Math", grade: 91 }]);
-    expect(db.getTranscript(id2)?.grades).toStrictEqual([{ course: "Math", grade: 87 }]);
+  it(`should attach different grades to different students`, async () => {
+    const id1 = await db.addStudent("Carol");
+    const id2 = await db.addStudent("Darol");
+    await db.addGrade(id1, "Math", 91);
+    await db.addGrade(id2, "Math", 87);
+    expect((await db.getTranscript(id1)).grades).toStrictEqual([{ course: "Math", grade: 91 }]);
+    expect((await db.getTranscript(id2)).grades).toStrictEqual([{ course: "Math", grade: 87 }]);
   });
 
-  it(`should permit multiple grades for a single class`, () => {
-    const id = db.addStudent("Eris");
-    db.addGrade(id, "Math", 91);
-    db.addGrade(id, "Math", 87);
-    expect(db.getTranscript(id).grades.sort((a, b) => a.grade - b.grade)).toStrictEqual([
+  it(`should permit multiple grades for a single class`, async () => {
+    const id = await db.addStudent("Eris");
+    await db.addGrade(id, "Math", 91);
+    await db.addGrade(id, "Math", 87);
+    expect((await db.getTranscript(id)).grades.sort((a, b) => a.grade - b.grade)).toStrictEqual([
       { course: "Math", grade: 87 },
       { course: "Math", grade: 91 },
     ]);
--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,7 @@
 // types.ts - types for the transcript service
 
 export type Course = string;
-export type StudentID = number;
+export type StudentID = string;
 export type StudentName = string;
 export type Student = { studentID: StudentID; studentName: StudentName };
 export type CourseGrade = { course: Course; grade: number };
